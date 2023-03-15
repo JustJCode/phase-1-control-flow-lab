172d5d3a8ad2ed4345bdce900b8de96729a13d77
Julian Victorio <julianv714@gmail.com>
2023-03-14 17:40:20 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(somevalue){
+  if (somevalue <= 400) {
+    return "This one is on me!"
+  }
+
+  else if (somevalue >= 400 && somevalue <= 2000){
+    return "That will be twenty bucks."
+  }
+
+  else if(somevalue > 2000 && somevalue <=2001){  
+    return "I will gladly take your thirty bucks."
+  } 
+
+  return "No can do."
+}
+function ternaryCheckCity(somecity){
+  return somecity === 'NYC'?'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(sometip){
+  switch(sometip){
+    case 'generous':
+      return "Thank you so much."
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    case 'not as generous':
+      return "Thank you."
+
+    default:
+      return "Bye."
+  }
 }
